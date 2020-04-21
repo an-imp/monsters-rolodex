9b955c09296bbd1486b9bf94bc4941fa80700133
Vicky Hu <buyifly@live.com>
2020-04-21 22:26:05 +1200

    Updates

---
+++ precache-manifest.1ff1779682bba5e57f2ae7cfa4df3b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e71c2d2c0305b45fb92a50fe7295d3c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9a637c67af10d0c7951",
+    "url": "/monsters-rolodex/static/css/main.5a991a3f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a637c67af10d0c7951",
+    "url": "/monsters-rolodex/static/js/main.4611674f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
